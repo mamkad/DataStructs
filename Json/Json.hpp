3478802f74df13450f8d537eed2ec0848e612d2b
mamkad <89218729+mamkad@users.noreply.github.com>
2022-09-16 13:02:51 +0300

    корректировка json

--- Json/Json.hpp
+++ Json/Json.hpp
@@ -1,69 +1,74 @@
 #pragma once
 
-//#include "../String/String.hpp"
-#include "../File/File.hpp"
+#include "String.hpp"
+//#include "../File/File.hpp"
 
 #include <map>
 #include <memory>
-#include <string>
-#include <iostream>
 
-using std::string;
 using std::map;
 using std::shared_ptr;
-using std::make_shared;
-using std::cout;
 
 namespace DataStructs
 {
+    // класс json-узла (ключ и указатель на значения для данного ключа
     struct JsonNode final
     {
-        JsonNode()
-        {
-            //cout << "JsonNode()\n";
-        }
+    public:
+        JsonNode();
+        JsonNode(JsonNode const&);
+        JsonNode(JsonNode&&) noexcept;
+        ~JsonNode();
 
-        ~JsonNode()
-        {
-            //cout << "~JsonNode()\n";
-        }
+    public:
+        JsonNode& operator=(JsonNode const&);
+        JsonNode& operator=(JsonNode&&) noexcept;
 
-        void setValue(string val)
-        {
-            value = val;
-        }
+    public:
+        shared_ptr<JsonNode> createChildNode(String const&); // создать дочерний узел для данного узла с ключом 
+        void setValue(String const&);                        // задать занчение по данному ключу
+        String& getValue();                                  // получить значение
+        void clear();                                        // очистить содержимое
 
-        shared_ptr<JsonNode> getNode(string key)
-        {
-            auto resIter = JsonNodes.insert({key, make_shared<JsonNode>()});
-            return resIter.first->second;
-        }
+    public:
+        bool emptyValue()  const noexcept;  // содежит ли узел значение
+        bool emptyChilds() const noexcept;  // имеет ли узел дочерние узлы
 
-        string value;
-        map<string, shared_ptr<JsonNode>> JsonNodes;
+    private:
+        String value_;                                   // значение
+        map<String, shared_ptr<JsonNode>> childNodes_;   // дочерние узлы
     };
 
     class Json final
     {
     private:
-        JsonNode headNode;
-        File jsonFile;
+        shared_ptr<JsonNode> rootNode_ = make_shared<JsonNode>(); // главнй корневой узел 
 
     public:
         Json();
-        Json(string const& fileName);
+        Json(String const&);
+        Json(Json const&);
+        Json(Json&&);
         ~Json();
 
     public:
-        JsonNode& get(string const& key);
-        JsonNode& set(string const& key);
+        Json& operator=(Json const&);
+        Json& operator=(Json&&);
 
     public:
-        void parseFromFile(string const& fileName);
-        void parseFromString(string const& jsonString); 
+        JsonNode& getByKey(String const&);   // получить значение по ключу
+        JsonNode& operator[](String const&); // получить значение по ключу
+
+    public:
+        void parseFromFile(String const&);
+        void parseFromString(String const&);
+        void clear();
+
+    public:
+        bool empty() const noexcept;
 
     private:
-        
+        static size_t extractValue(String const&, String&, char, size_t, function<bool(char)> const&, function<void()> const&);
+        static size_t parse(shared_ptr<JsonNode>&, String const&, size_t);
     };
 }
-
